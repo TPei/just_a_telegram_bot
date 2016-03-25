c7c8d4e913b1b00369dcaacdcf9e3a1129acb5b3
TPei <t.s.peikert@gmail.com>
2016-03-25 23:32:54 +0100

    lol it works

--- main.rb
+++ main.rb
@@ -3,3 +3,13 @@ require 'dotenv'
 Dotenv.load
 
 token = ENV['BOT_TOKEN']
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/swag'
+      bot.api.send_message(chat_id: message.chat.id,
+                           text: 'swag')
+    end
+  end
+end
