186666daa346eb7eff84b178465194cde19d7081
TPei <t.s.peikert@gmail.com>
2016-03-31 12:58:24 +0200

    removes debugging residue

--- bots/wikipedia_bot.rb
+++ bots/wikipedia_bot.rb
@@ -8,8 +8,6 @@ class WikipediaBot
     url = 'https://en.wikipedia.org/wiki/Special:Random'
     final_url = ''
     RestClient.get(:url => url, :verify_ssl => false){ |response, request, result, &block|
-      require 'pry'
-      binding.pry
       if [301, 302, 307].include? response.code
         response.follow_redirection(request, result, &block)
       else
