20b6e7bc3603298e6374e853b6c7b0388efaa448
TPei <t.s.peikert@gmail.com>
2016-03-31 12:59:41 +0200

    cleanup

--- bots/wikipedia_bot.rb
+++ bots/wikipedia_bot.rb
@@ -7,13 +7,12 @@ class WikipediaBot
   def check
     url = 'https://en.wikipedia.org/wiki/Special:Random'
     final_url = ''
-    RestClient.get(:url => url, :verify_ssl => false){ |response, request, result, &block|
+    RestClient.get(:url => url, :verify_ssl => false) do |response, request, result, &block|
       if [301, 302, 307].include? response.code
         response.follow_redirection(request, result, &block)
-      else
-        final_url = request.url
       end
-    }
+      final_url = request.url
+    end
     final_url
   end
 end
