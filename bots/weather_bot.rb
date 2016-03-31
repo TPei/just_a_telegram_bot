ab93dc082d9abbe914864f2a7ce91d87326e7cfd
TPei <t.s.peikert@gmail.com>
2016-03-31 12:29:27 +0200

    sounded stupid

--- bots/weather_bot.rb
+++ bots/weather_bot.rb
@@ -25,7 +25,7 @@ class WeatherBot
   def weather_forecast
     url = "#{BASE_URL}forecast?q=#{@city},#{@country}&appid=#{@api_key}"
     response = parse(RestClient.get(url))
-    response = response['list'][time_number]
+    response = response['list'][time_index]
     celcius = k_to_c(response['main']['temp'])
     trait = response['weather'][0]['main']
     "Im wunderschoenen #{@city} werden es #{@time} #{celcius} Grad mit #{trait}"
@@ -41,7 +41,7 @@ class WeatherBot
     (k - 273.15).round(1)
   end
 
-  def time_number
+  def time_index
     case @time
     when 'today'
       1
