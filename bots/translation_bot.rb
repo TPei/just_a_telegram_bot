93f5c07420af3c5c9ad28ee6d45831a9e0682df6
TPei <t.s.peikert@gmail.com>
2016-04-14 08:20:27 +0200

    update translation urls

--- bots/translation_bot.rb
+++ bots/translation_bot.rb
@@ -3,8 +3,9 @@ require 'dotenv'
 Dotenv.load
 
 class TranslationBot
-  URL = 'https://translate.yandex.net/api/v1.5/tr.json/detect?'.freeze
-  TRANSLATE_URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate?'.freeze
+  URL = 'https://translate.yandex.net/api/v1.5/tr.json/'.freeze
+  DETECT = 'detect?'.freeze
+  TRANSLATE = 'translate?'.freeze
 
   def initialize(text)
     @text = text
@@ -12,10 +13,10 @@ class TranslationBot
 
   def translate
     api_key = ENV['YANDEX_KEY']
-    response = JSON.parse(RestClient.get("#{URL}&key=#{api_key}&text=#{@text}"))
+    response = JSON.parse(RestClient.get("#{URL}#{DETECT}&key=#{api_key}&text=#{@text}"))
     lang = response['lang']
 
-    response = JSON.parse(RestClient.get("#{TRANSLATE_URL}&key=#{api_key}&text=#{@text}&lang=#{lang}-en"))
+    response = JSON.parse(RestClient.get("#{URL}#{TRANSLATE}&key=#{api_key}&text=#{@text}&lang=#{lang}-en"))
     response['text'][0]
   end
 end
